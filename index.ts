6dbbbb604b2c5525e3eaafb39fed303207743a70
ThiagoMartins2611 <thiagogames2611@gmail.com>
2025-04-14 22:56:16 -0400

    formulario

--- index.ts
+++ index.ts
@@ -1,14 +1,20 @@
 import express from 'express';
 import path from 'path';
 
-const app = express();
-const port = 8000;
 
-app.get('/', (req, res)=>{
+const app = express();
+const port:number = 8000;
+
+app.use(express.json())
+app.use(express.static(path.join(__dirname, './src')))
+
+
+app.get('/', (req:express.Request, res:express.Response)=>{
 
     res.sendFile(path.join(__dirname, './src/index.html'))
-
+    
 });
+
 
 
 
