d389a99c7f7d382874568e3b5b8f6bd84a4d7180
ThiagoMartins2611 <thiagogames2611@gmail.com>
2025-04-14 20:29:35 -0400

    New page create

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("tere lindu")
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res)=>{
+
+    res.sendFile(path.join(__dirname, './src/index.html'))
+
+});
+
+
+
+app.listen(port, ()=>{
+    console.log("servidor rodadno na porta 8000")
+})
